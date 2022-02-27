9587d6834104d44ee5aacd2141434eca77dcf3a4
venkatram64 <venkat.veerareddy@hotmail.com>
2022-02-27 10:21:28 +0530

    init commit

--- emp-crud-with-class-redux-app/src/components/Navbar.js
+++ emp-crud-with-class-redux-app/src/components/Navbar.js
@@ -5,15 +5,6 @@ const Navbar = () => {
   return (
       <nav className='navbar navbar-expand-lg navbar-light bg-light py-2'>
           <Link to="/" className='navbar-brand ml-5'>React Redux App</Link>
-          <Link className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Employee
-            </Link>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <Link to="/" className='navbar-brand ml-5'>List Employee</Link>
-              <Link to="/add" className='navbar-brand ml-5'>Add Employee</Link>              
-            </div>
-          </Link>
       </nav>
   )
 };
